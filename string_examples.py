79454e3fd4f385ed8bc598b65329a047dd90e808
Kimsi <kimsi@Kimsis-MacBook-Pro.local>
2019-09-06 17:23:18 +0530

    My first Python commit

---
+++ string_examples.py
@@ -0,0 +1,5 @@
+print('hello world')
+my_variable1 = 'Jinnah'
+print ('The first PM of Pakistan was ' + my_variable1)
+print('My name is {j} and my age is {a}'.format(j='John',a='75'))
+print('Using Indexing we have the first item as {0} and second as {1}'.format('Toilet Paper', 'Plunger'))
