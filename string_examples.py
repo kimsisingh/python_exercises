2fbe8007425ca870032f959523f8b2916760a6b6
Kimsi <kimsi@Kimsis-MacBook-Pro.local>
2019-09-08 22:15:38 +0530

    Added more String exercises

--- string_examples.py
+++ string_examples.py
@@ -3,3 +3,45 @@ my_variable1 = 'Jinnah'
 print ('The first PM of Pakistan was ' + my_variable1)
 print('My name is {j} and my age is {a}'.format(j='John',a='75'))
 print('Using Indexing we have the first item as {0} and second as {1}'.format('Toilet Paper', 'Plunger'))
+print(2+2) #Addition
+print('2' + '3') #String Concatenation
+print(7/4) # Division
+print(7%4) # Remainder to check if even or odd
+print(2 ** 3) # 2 to the power of 3
+print(2 + 10 * 10 + 3 ) # basic order of operations
+print((2 + 10) * (10 + 3)) # the way you want
+my_dogs = 2
+my_dogs = ["Sammy", "Frankie"] # totally OK as Dynamic Typing in Python
+#No Restrictions on data type . In other languages, there is static typing
+# Pros of Dynamic : Saves time, makes code readbale
+# Cons : may result in bugs due to the unexpected data type.
+
+my_str_kim = "Hello, My name is Kimsi Singh"
+print("lalalalalalal")
+print(my_str_kim)
+my_num=5
+print ("Original my_num is")
+print (my_num) #Assign names and variables
+my_num = 15
+print ("my_num now is " )
+print( my_num)
+my_num = (my_num + my_num)
+print("my_num + my_num now is " )
+print(my_num) 
+my_num = "Well, My_num, now is a String!"
+print("Whats my_num now? " + my_num)
+print("Indexing Trial, letter at index zero " + my_num[0])
+print("Indexing : Fetch the letter S at index 23 : " + my_num[23])
+print("Reverse Indexing : Fetching letter S again at index -7 : " + my_num[-7])
+print("String slicing by grabbing the string from My_num till the end: " + my_num[6:])
+print("String slicing by grabbing the string from beginning till well: " + my_num[:4])
+print("Step or jump by 2: " + my_num[:: 2])
+print ("Reverse the entire string " +my_num[::-1])
+#print(" Whats the type of my_num ")
+
+
+
+
+
+
+
