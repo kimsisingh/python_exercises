0a3a5ed98f394ee1b79105b55ccf802d159c01fc
Kimsi <kimsi@Kimsis-MacBook-Pro.local>
2019-09-12 23:42:08 +0530

    Exercise for Boolean in Python

---
+++ boolean_examples.py
@@ -0,0 +1,5 @@
+# Booleans in Python i.e True or False
+# upper case T and upper case F
+print(f"Type of this object (2>5) : {type(2>5)}")
+print(f"Is 2 greater than 5 :  {2>5}")
+print(f"Is 3 equal to 3 :  {3==3}")
