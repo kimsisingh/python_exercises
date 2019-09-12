3fb24c21fc4ae04cb66b9c2fa165da7e54f0b5fa
Kimsi <kimsi@Kimsis-MacBook-Pro.local>
2019-09-12 23:47:42 +0530

    Added None keyword example

--- boolean_examples.py
+++ boolean_examples.py
@@ -3,3 +3,15 @@
 print(f"Type of this object (2>5) : {type(2>5)}")
 print(f"Is 2 greater than 5 :  {2>5}")
 print(f"Is 3 equal to 3 :  {3==3}")
+# Assign None Keyword to an object for whom you have not decided what the data type
+# is going to be
+# If you don't say None, it will throw an error saying that the 
+# object is not defined
+# The following throws error that be is not defined
+#(f"What is b :  {b}")
+#Assign the keyword None to object b
+b = None
+print(f"What is b now assigned None to it :  {b}")
+
+
+
